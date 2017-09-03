352a05b423877f14d3efb4c1ecbe3a9fc8b066de
szzc <zzsc@o2.pl>
2017-09-03 22:23:55 +0200

    Delete connection.php

--- php/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$link = mysqli_connect("mysql.superhost.pl", "xdesign_ia", "uS02Y37fcJ1x", "xdesign_ironaltar") 
-        or die("Some error occurred during connection " . mysqli_error($link));
-?>
